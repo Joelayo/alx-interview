23ed9fb533b05fbac2ea456c2a4dd65676951c6d
Joelayo <joetech.ooj@gmail.com>
2023-01-06 03:14:10 +0100

    Locked boxes unlocked

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,25 +4,22 @@
 
 
 def canUnlockAll(boxes):
-    '''Checks if all the boxes in a list of boxes containing the keys
-    (indices) to other boxes can be unlocked given that the first
-    box is unlocked.
-    '''
+    # Set of boxes that have been checked
     checked = set()
-    # Queue of boxes to check
-    queue = [0]
+    # Stack of boxes to check
+    stack = [0]
 
     # Repeat the process until all boxes have been checked
-    while queue:
+    while stack:
         # Get the next box to check
-        box = queue.pop(0)
+        box = stack.pop()
         # Add the box to the set of checked boxes
         checked.add(box)
         # Check if the box contains any keys
         for key in boxes[box]:
-            # If the key corresponds to an unchecked box, add it to the queue
+            # If the key corresponds to an unchecked box, add it to the stack
             if key not in checked:
-                queue.append(key)
+                stack.append(key)
 
     # Return whether all boxes have been checked
     return len(checked) == len(boxes)
