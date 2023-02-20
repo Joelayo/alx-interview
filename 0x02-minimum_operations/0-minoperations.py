945c87ab130f091800d7dc53714079ad386a9b40
Joelayo <joetech.ooj@gmail.com>
2023-02-20 03:32:44 +0100

    Project Files

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-"""
-Minimum OPerations
-"""
+'''The minimum operations coding challenge.
+'''
 
 
 def minOperations(n):
-    if not isinstance(n, int) or n < 1:
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
         return 0
     ops_count = 0
     clipboard = 0
     done = 1
     while done < n:
-        if clipboard == 0 or n % done != 0:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
             # copy all and paste
             clipboard = done
             done += clipboard
             ops_count += 2
-        else:
+        elif clipboard > 0:
             # paste
             done += clipboard
             ops_count += 1
+    # print('')
     return ops_count
